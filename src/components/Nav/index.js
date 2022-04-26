a299cc6bf23fd62759896c9d777a5dbc7d51b076
REX <cssmith431@yahoo.com>
2022-04-26 11:31:59 -0400

    About/Nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function Nav() {
+    const categories = [
+        {
+            name: 'commercial',
+            description:
+            "Phtos of grocery stores, food trucks, and other commercial projects",
+        },
+        {name: 'portraits', description: "Portraits of people in my life"},
+        {name: 'food', description: 'Delicious delicacies'},
+        {name: 'landscape', description: "fields, farmhouses, waterfalls"},
+    ];
+    function categorySelected(name){
+        console.log(`${name} clicked`)
+    }
+
+    return (
+        <header data-testid="header" className="flex-row px-1">
+  <h2>
+    <a href="/">
+      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    </a>
+  </h2>
+  <nav>
+    <ul className="flex-row">
+      <li className="mx-2">
+        <a href="#about">
+          About me
+        </a>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+       {categories.map((category) => (
+        <li
+          className="mx-1"
+          key={category.name}
+        >
+          <span onClick={categorySelected}>
+            {category.name}
+          </span>
+        </li>
+      ))}
+    </ul>
+  </nav>
+</header>
+    );
+}
+
+
+
+export default Nav;
+
+
